032e4bbff7ee3f95b8db5f5fd9d1196512237a11
Joachim Stolberg <joe.stolberg@gmx.de>
2022-03-27 20:35:32 +0200

    Enable import of files

--- bcomp/bscanner.lua
+++ bcomp/bscanner.lua
@@ -9,6 +9,23 @@
   See LICENSE.txt for more information
 
   Scanner/Tokenizer
+  
+  The Scanner is called recursive to handle import files.
+  
+  It returns a list with tokens according to:
+  
+  {type = T_SOURCE, val = "while(1)", lineno = 5}
+  {type = T_IDENT, val = "while"}
+  {type = T_BRACE, val = "("}
+  {type = T_NUMBER, val = 1}
+  {type = T_BRACE, val = ")"}
+  
+  {type = T_SOURCE, val = "_asm_ {", lineno = 6}
+  {type = T_ASMSRC, val = "move A, #1", lineno = 7}
+  
+  {type = T_SOURCE, val = 'import "test.c"', lineno = 8}
+  {type = T_NEWFILE, val = "test.c"}
+  {type = T_ENDFILE, val = "test.c"}  
 
 ]]--
 
@@ -27,10 +44,23 @@ local T_NUMBER  = 2
 local T_OPERAND = 3
 local T_BRACE   = 4
 local T_STRING  = 5
+local T_ASMCODE = 6
+local T_SRCCODE = 7
+local T_NEWFILE = 8
+local T_ENDFILE = 9
+
+local lTypeString = {"ident", "number", "operand", "brace", "string", "asm code", "src code", "new file", "end file"}
+local lToken = {}
+local tScannedFiles = {}
 
 local strfind = string.find
 local strsub  = string.sub
 local tinsert = table.insert
+
+local function file_ext(filename)
+	local _, ext = unpack(string.split(filename, ".", true, 1))
+	return ext
+end
 
 local function split_into_lines(text)
 	local list = {}
@@ -61,18 +91,34 @@ end
 local BScan = vm16.BGen:new({})
 
 function BScan:bscan_init()
-	self.ltok = {}
 	self.tk_idx = 1
-	self.co = coroutine.create(self.line_produce)
+	self.nested_calls = self.nested_calls or 0
+	-- Reset global tables only once
+	if self.nested_calls == 0 then
+		lToken = {}
+		tScannedFiles = {}
+	end
 end
 
-function BScan:scanner(text)
+function  BScan:import_file(filename)
+	if tScannedFiles[filename] then
+		return  -- already imported
+	end
+	tScannedFiles[filename] = true
+	local is_asm_code = file_ext(filename) == "asm"
+	local i = vm16.BScan:new({
+		readfile = self.readfile, 
+		is_asm_code = is_asm_code,
+		nested_calls = self.nested_calls + 1
+	})
+	i:bscan_init()
+	i:scanner(filename)
+end
+
+function BScan:tokenize(text)
 	local idx = 1
 	local size = #text
-	-- If necessary, save the last value (needed for tk_next)
-	self.ltok = {self.ltok[self.tk_idx]}
-	self.tk_idx = 1
-
+	
 	while idx <= size do
 		local ch = text:sub(idx, idx)
 		if ch:match(SPACE) then
@@ -80,95 +126,118 @@ function BScan:scanner(text)
 			idx = idx + #space
 		elseif ch:match(IDENT1) then
 			local ident = text:match(IDENT2, idx)
-			table.insert(self.ltok, {type = T_IDENT, val = ident})
+			if ident == "import" then
+				self.is_import_line = true
+			elseif ident == "_asm_" then
+				self.is_asm_code = true
+				return
+			else
+				table.insert(lToken, {type = T_IDENT, val = ident})
+			end
 			idx = idx + #ident
 		elseif ch == "0" and text:sub(idx + 1, idx + 1) == "x" then
 			idx = idx + 2
 			local number = text:match(HEXNUM, idx)
-			table.insert(self.ltok, {type = T_NUMBER, val = tonumber(number, 16) or 0})
+			table.insert(lToken, {type = T_NUMBER, val = tonumber(number, 16) or 0})
 			idx = idx + #number
 		elseif ch:match(NUMBER) then
 			local number = text:match(NUMBER, idx)
-			table.insert(self.ltok, {type = T_NUMBER, val = tonumber(number) or 0})
+			table.insert(lToken, {type = T_NUMBER, val = tonumber(number) or 0})
 			idx = idx + #number
 		elseif ch:match(OPERAND) then
 			local operand = text:match(OPERAND, idx)
 			if operand:sub(1, 2) == "//" then -- EOL comment
 				break
 			end
-			table.insert(self.ltok, {type = T_OPERAND, val = operand})
+			table.insert(lToken, {type = T_OPERAND, val = operand})
 			idx = idx + #operand
 		elseif ch:match(BRACE) then
-			table.insert(self.ltok, {type = T_BRACE, val = ch})
+			table.insert(lToken, {type = T_BRACE, val = ch})
 			idx = idx + 1
 		elseif ch == "'" and text:match(CHAR, idx) then
 			local char = text:match(CHAR, idx)
 			local val = char_to_val(char)
-			table.insert(self.ltok, {type = T_NUMBER, val = val})
+			table.insert(lToken, {type = T_NUMBER, val = val})
 			idx = idx + #char + 2
 		elseif ch == '"' and text:match(STRING, idx) then
 			local str = text:match(STRING, idx)
-			table.insert(self.ltok, {type = T_STRING, val = str})
+			if self.is_import_line then
+				self.is_import_line = nil
+				self:import_file(string.sub(str, 2, -2))
+			else
+				table.insert(lToken, {type = T_STRING, val = str})
+			end
 			idx = idx + #str
 		else
-			error(string.format("Syntax error at '%s'", ch))
+			self:error_msg(string.format("Invalid character '%s'", ch))
 		end
 	end
 end
 
-function BScan:line_produce()
-	--require('mobdebug').on()
-	for lineno, line in ipairs(split_into_lines(self.text)) do
+function BScan:scanner(filename)
+	self.filename = filename
+	self.lineno = 0
+	if self.nested_calls > 10 then
+		self:error_msg("Maximum number of nested imports exceeded")
+	end
+	table.insert(lToken, {type = T_NEWFILE, val = filename})
+
+	local text = self.readfile(self.pos, filename)
+	for lineno, line in ipairs(split_into_lines(text)) do
+		local size = #line
 		self.lineno = lineno
-		if self.scanner_raw_mode then
-			coroutine.yield(line)
-		elseif line:trim() ~= "" then
-			self:add_line(string.format(";%4d: %s", lineno, line))
-			self:scanner(line)
-			coroutine.yield(true)
+		if self.is_asm_code then
+			if line:trim() == "}" then
+				self.is_asm_code = false
+				table.insert(lToken, {type = T_SRCCODE, val = line, lineno = lineno})
+			else
+				table.insert(lToken, {type = T_ASMCODE, val = line, lineno = lineno})
+			end
+		else
+			table.insert(lToken, {type = T_SRCCODE, val = line, lineno = lineno})
+			self:tokenize(line)
 		end
 	end
+	
+	table.insert(lToken, {type = T_ENDFILE, val = filename})
+	return lToken
 end
 
 function BScan:tk_match(ttype)
-	while self.tk_idx > #self.ltok do
-		local res = coroutine.resume(self.co, self)
-		if not res then break end
-	end
-	local tok = self.ltok[self.tk_idx] or {}
-	if tok.val == self.break_ident then
-		print("Stop")
-	end
+	local tok = lToken[self.tk_idx] or {}
 	if not ttype or ttype == tok.type or ttype == tok.val then
 		self.tk_idx = self.tk_idx + 1
 		return tok
 	end
-	--print(string.format("Syntax error at '%s', '%s' expected", tok.val or "", ttype or ""))
-	error(string.format("Syntax error at '%s', '%s' expected", tok.val or "", ttype or ""))
+	self:error_msg(string.format("Syntax error at '%s', '%s' expected", tok.val or "", ttype or ""))
 end
 
 function BScan:tk_peek()
-	while self.tk_idx > #self.ltok do
-		local res = coroutine.resume(self.co, self)
-		if not res then break end
-	end
-	return self.ltok[self.tk_idx] or {}
+	return lToken[self.tk_idx] or {}
 end
 
 function BScan:tk_next()
-	while self.tk_idx + 1 > #self.ltok do
-		local res = coroutine.resume(self.co, self)
-		if not res then break end
-	end
-	return self.ltok[self.tk_idx + 1] or {}
+	return lToken[self.tk_idx + 1] or {}
 end
 
-function BScan:tk_rawline()
-	self.scanner_raw_mode = true
-	local res, line = coroutine.resume(self.co, self)
-	self.scanner_raw_mode = false
-	return res and line
+function BScan:dump(lToken)
+	local out = {}
+	
+	for idx,tok in ipairs(lToken) do
+		if tok.type == T_SRCCODE or tok.type == T_ASMCODE then
+			out[idx] = string.format('%8s: (%d) "%s"', lTypeString[tok.type], tok.lineno, tok.val)
+		elseif tok.type == T_NEWFILE then
+			out[idx] = string.format('%8s: <<<<<<<< "%s" <<<<<<<<', lTypeString[tok.type], tok.val)
+		elseif tok.type == T_ENDFILE then
+			out[idx] = string.format('%8s: >>>>>>>> "%s" >>>>>>>>', lTypeString[tok.type], tok.val)
+		else
+			out[idx] = string.format('%8s: "%s"', lTypeString[tok.type], tok.val)
+		end
+	end
+	
+	return table.concat(out, "\n")
 end
+
 
 vm16.BScan = BScan
 
@@ -184,3 +253,7 @@ vm16.T_NUMBER  = T_NUMBER
 vm16.T_OPERAND = T_OPERAND
 vm16.T_BRACE   = T_BRACE
 vm16.T_STRING  = T_STRING
+vm16.T_ASMCODE = T_ASMCODE
+vm16.T_SRCCODE = T_SRCCODE
+vm16.T_NEWFILE = T_NEWFILE
+vm16.T_ENDFILE = T_ENDFILE
