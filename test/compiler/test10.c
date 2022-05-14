a5857f4367c330aa6f1d61d8ca4bd27aa6d7d7cc
Joachim Stolberg <joe.stolberg@gmx.de>
2022-05-14 20:08:28 +0200

    Add break, continue, goto statements

---
+++ test/compiler/test10.c
@@ -0,0 +1,40 @@
+import "stdio.asm"
+
+func test1() {
+  var i = 0;
+  var j = 0;
+
+  for(j = 0; j < 5; j++) {
+    for(i = 0; i < 5; i++) {
+      putnum(i);
+      if(i < 2) {
+        continue;
+      }
+      putstr("i >= 2");
+      if(i == 4) {
+        break;
+      }
+    }
+  }
+
+  i = 0;
+  while(1) {
+    if(i>3) {
+      break;
+    }
+    putnum(i);
+    i++;
+  }
+}
+
+func test2() {
+  putstr("Should not happen");
+}
+
+func main() {
+  test1();
+
+  goto exit;
+  test2();
+  exit:
+}
