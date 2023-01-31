b2dc15f882c17af2d3760507ee0a4d4280bfd988
Joachim Stolberg <joe.stolberg@gmx.de>
2023-01-31 21:48:00 +0100

    Add labels as values

---
+++ test/compiler/test18.c
@@ -0,0 +1,30 @@
+func init() {
+}
+
+func foo(a, b) {
+  return a *2 + b;
+}
+
+func loop() {
+  var addr = &&mark3;
+
+  // 	https://gcc.gnu.org/onlinedocs/gcc/Labels-as-Values.html
+  goto *addr;
+
+  goto mark1;
+  
+  while(1) {
+    mark1:
+      system(0, 0x31);
+      break;
+
+    mark2:
+      system(0, 0x32);
+      break;
+
+    mark3:
+      system(0, 0x33);
+      goto mark2;
+      break;
+  }
+}
